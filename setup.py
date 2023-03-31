c67a17ced5ee6663be17cb5855763a296d656c9e
devki <you@example.com>
2023-03-31 17:31:56 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="cencus income", 
+      version="0.0.1",
+      author="devki",
+      author_email="dzunzurwad@gmail.com",
+      packages=find_packages(),
+      install_requires=["numpy", "pandas", "flask"]
+)
